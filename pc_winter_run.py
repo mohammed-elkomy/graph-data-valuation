e9bf1a41689f0d1a5a7aebef43127ae165e91b45
mohammed_elkomy <mohammed.a.elkomy@gmail.com>
2025-01-06 20:16:23 +0200

    Mon Jan  6 08:16:23 PM EET 2025

--- pc_winter_run.py
+++ pc_winter_run.py
@@ -287,7 +287,7 @@ def get_subgraph_data(data_edge_index, mask):
 
     # Nodes to be considered
     edge_index = data_edge_index.clone()
-    print(mask.shape, mask.nonzero().shape)
+    print(mask.shape, mask[:, 0].nonzero().shape)
     nodes = mask.nonzero().view(-1)
 
     # Extract the edges for these nodes
