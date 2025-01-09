932eadcf0b469b5808b66a342e09989e37e2dbc6
mohammed_elkomy <mohammed.a.elkomy@gmail.com>
2025-01-09 20:15:33 +0200

    Thu Jan  9 08:15:33 PM EET 2025

--- node_drop_large.py
+++ node_drop_large.py
@@ -163,8 +163,8 @@ def get_subgraph_data(data, mask):
 # Find matching files for PC-Winter results
 matching_files = []
 for filename in os.listdir(directory):
+    print(filename)
     if pattern.match(filename):
-        print(filename)
         matching_files.append(filename)
 filenames = matching_files[:ratio]
 ratio = min(len(filenames), ratio)  # Limit the number of files to process
