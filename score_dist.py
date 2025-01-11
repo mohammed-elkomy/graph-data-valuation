e204e07c32669b47a7e641ac3a55856cdca54f30
mohammed_elkomy <mohammed.a.elkomy@gmail.com>
2025-01-11 13:46:59 +0200

    Sat Jan 11 01:46:59 PM EET 2025

--- score_dist.py
+++ score_dist.py
@@ -67,7 +67,7 @@ def analyze_dist(values, x_axis, y_axis, title, filename):
     filtered_values = [v for v in values if lower_bound <= v <= upper_bound]
 
     # Plot the histogram with filtered values
-    n, bins, patches = plt.hist(filtered_values, bins=1000, alpha=0.75, edgecolor='black')
+    n, bins, patches = plt.hist(filtered_values, bins=10000, alpha=0.75, edgecolor='black')
 
     # Set y-ticks to show 20 evenly spaced ticks
     y_max = n.max()  # Maximum density value from the histogram
