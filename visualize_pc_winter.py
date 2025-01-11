29630e25b1989554a7a0f8bad3be5d710217ef3b
mohammed_elkomy <mohammed.a.elkomy@gmail.com>
2025-01-11 21:53:55 +0200

    Sat Jan 11 09:53:55 PM EET 2025

--- visualize_pc_winter.py
+++ visualize_pc_winter.py
@@ -49,7 +49,8 @@ for file_path in sorted(glob.glob(pattern)):
     files_per_dump_count[dump_count].append(file_path)
 
 for dump_count, matched_files in files_per_dump_count.items():
-    assert len(matched_files) == 10, f"failed to get parts of \n {'\n'.join(matched_files)}"
+    sub = "\n".join(matched_files)
+    assert len(matched_files) == 10, f"failed to get parts of \n{sub}"
 
 for dump_count, data_group in datagroups.items():
     dump_count = int(dump_count)
